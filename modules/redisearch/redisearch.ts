15147c453f53bec3d051f6379314a00b9807d097
Ashleigh Plummer <67503626+ashleighplummer@users.noreply.github.com>
2021-11-04 11:43:29 +0000

    removed handleResponse in aggregate function
    this.handleResponse() not returning correct response format.

--- modules/redisearch/redisearch.ts
+++ modules/redisearch/redisearch.ts
@@ -74,7 +74,7 @@ export class Redisearch extends Module {
     async aggregate(index: string, query: string, parameters?: FTAggregateParameters): Promise<[number, ...Array<string[]>]> {
         const command = this.searchCommander.aggregate(index, query, parameters);
         const response = await this.sendCommand(command);
-        return this.handleResponse(response);
+        return response;
     }
 
     /**
